6aec8281d412de5b719e3206d3ada2988afbfa61
Diego del Valle <diegodelvalle333@gmail.com>
2018-07-15 18:32:42 -0300

    Libreria de Strings de consultas
    Se agregaron 2 consultas:
    1- Consulta de usuarios nuevos utilizando la cedula venezolana como
    criterio de busqueda
    2- Consulta de usuarios nuevos utilizando el nombre y apellido como
    criterio de busqueda

---
+++ JavaAplication/src/logicaPersistencia/dbAccess/QueryStore.java
@@ -0,0 +1,20 @@
+package logicaPersistencia.dbAccess;
+
+public class QueryStore {
+	public String select_NuevosUsuarios_Lista__CIVenezolana(){
+		//String de consulta
+		//Permite obtener una lista de los nuevos usuarios registrados
+		//Utilizando la cedula venezolana como criterio de busqueda
+		String query = "SELECT * FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND D.CI_VENEZOLANA = (?)";
+		return query;
+	}
+	
+	public String select_NuevosUsuarios_Lista__NombreApellido(){
+		//String de consulta
+		//Permite obtener una lista de los nuevos usuarios registrados
+		//Utilizando el nombre y apellido como criterio de busqueda
+		String query = "SELECT * FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND CONCAT(PRIMER_NOMBRE,' ',PRIMER_APELLIDO) LIKE (?)";
+		return query;
+	}
+	
+}
