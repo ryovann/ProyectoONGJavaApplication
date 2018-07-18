7cbd84b56072f72f3fad487a837087ce7a70386c
Diego del Valle <diegodelvalle333@gmail.com>
2018-07-18 13:43:35 -0300

    Se modificaron los string de consulta de nuevos usuarios

--- JavaAplication/src/logicaPersistencia/dbAccess/QueryStore.java
+++ JavaAplication/src/logicaPersistencia/dbAccess/QueryStore.java
@@ -5,7 +5,7 @@ public class QueryStore {
 		//String de consulta
 		//Permite obtener una lista de los nuevos usuarios registrados
 		//Utilizando la cedula venezolana como criterio de busqueda
-		String query = "SELECT * FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND D.CI_VENEZOLANA = (?)";
+		String query = "SELECT P.id_persona,ci_venezolana, primer_nombre, primer_apellido, motivo_contacto FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND D.CI_VENEZOLANA = (?)";
 		return query;
 	}
 	
@@ -13,7 +13,7 @@ public class QueryStore {
 		//String de consulta
 		//Permite obtener una lista de los nuevos usuarios registrados
 		//Utilizando el nombre y apellido como criterio de busqueda
-		String query = "SELECT * FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND CONCAT(PRIMER_NOMBRE,' ',PRIMER_APELLIDO) LIKE (?)";
+		String query = "SELECT P.id_persona,ci_venezolana, primer_nombre, primer_apellido, motivo_contacto FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND CONCAT(PRIMER_NOMBRE,' ',PRIMER_APELLIDO) LIKE (?)";
 		return query;
 	}
 	
