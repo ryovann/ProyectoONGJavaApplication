e7b3666871a8c967be68aabb67aba7fb5b538d2f
Diego del Valle <diegodelvalle333@gmail.com>
2018-07-22 21:21:43 -0300

    Se cambiaron los sql de busqueda de usuario

--- JavaAplication/src/logicaPersistencia/dbAccess/QueryStore.java
+++ JavaAplication/src/logicaPersistencia/dbAccess/QueryStore.java
@@ -1,19 +1,19 @@
 package logicaPersistencia.dbAccess;
 
 public class QueryStore {
-	public String select_NuevosUsuarios_Lista__CIVenezolana(){
+	public String select_Lista__CIVenezolana(){
 		//String de consulta
 		//Permite obtener una lista de los nuevos usuarios registrados
 		//Utilizando la cedula venezolana como criterio de busqueda
-		String query = "SELECT P.id_persona,ci_venezolana, primer_nombre, primer_apellido, motivo_contacto FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND D.CI_VENEZOLANA = (?)";
+		String query = "SELECT P.id_persona,ci_venezolana, primer_nombre, primer_apellido, motivo_contacto FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = (?) AND D.CI_VENEZOLANA = (?)";
 		return query;
 	}
 	
-	public String select_NuevosUsuarios_Lista__NombreApellido(){
+	public String select_Lista__NombreApellido(){
 		//String de consulta
 		//Permite obtener una lista de los nuevos usuarios registrados
 		//Utilizando el nombre y apellido como criterio de busqueda
-		String query = "SELECT P.id_persona,ci_venezolana, primer_nombre, primer_apellido, motivo_contacto FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = 0 AND CONCAT(PRIMER_NOMBRE,' ',PRIMER_APELLIDO) LIKE (?)";
+		String query = "SELECT P.id_persona,ci_venezolana, primer_nombre, primer_apellido, motivo_contacto FROM PERSONA P INNER JOIN DOCUMENTOS D ON D.ID_PERSONA = P.ID_PERSONA WHERE P.ESTADO = (?) AND CONCAT(PRIMER_NOMBRE,' ',PRIMER_APELLIDO) LIKE (?)";
 		return query;
 	}
 	public String select_ListaPaises(){
@@ -67,5 +67,6 @@ public class QueryStore {
 		String query = "INSERT INTO familia_persona (id_persona,vive_con,vino_con,cantidad_hijos,cant_hijos_extranjero) VALUES ((?),(?),(?),(?),(?))";
 		return query;
 	}
+	
 	
 }
