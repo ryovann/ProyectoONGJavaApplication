1d8c14ec94a1dfbda14c8323ced4616d782f19b5
Diego del Valle <diegodelvalle333@gmail.com>
2018-07-22 21:24:21 -0300

    Se agregaron funcionalidades y ligeros cambios a otras
    Se agrego funcion de ver informacion de usuarios y eliminar usuario
    Se modifico la funcionalidad de busqueda de usuario

--- JavaAplication/src/grafica/controller/MainWindow_Controller.java
+++ JavaAplication/src/grafica/controller/MainWindow_Controller.java
@@ -7,8 +7,9 @@ import javax.swing.table.DefaultTableModel;
 
 import grafica.window.FormularioRegistro;
 import grafica.window.TableModel;
+import grafica.window.VerInformacion;
 import logicaPersistencia.facade.Facade;
-import logicaPersistencia.valueObject.ValueObject_UsuariosNuevosBusqueda;
+import logicaPersistencia.valueObject.ValueObject_UsuariosBusqueda;
 
 public class MainWindow_Controller {
 	public static MainWindow_Controller instancia =null;
@@ -19,55 +20,57 @@ public class MainWindow_Controller {
 		}
 		return instancia;
 	}
-	public DefaultTableModel btnBuscarFunction(int queryType, String value) {
-		// queryType es un int que se utiliza para identificar
-		// si se utilizo la cedula venezolana o otro criterio
-		//columnNames guarda los nombres de las columnas a mostrar en la tabla
-		String[] columnNames = { "Cedula venezolana", "Primer nombre", "Apellido", "Motivo de Contacto" };
-		//Creo una tabla default a la cual no le agrego filas pero si columnas, las cuales esatn en ColumnNames
-		DefaultTableModel model = new TableModel(null, columnNames);
-		//Creo un array que guardara temporalmente los datos que van a ser agregados a el modelo
-		String[] rows = new String[4];
-		//Si los datos son del tipo correcto se ejecuta el bloque if
-		if (verifyInteger(value, queryType)) {
-			// este objeto guarda los datos y se pasan a facade
-			ValueObject_UsuariosNuevosBusqueda valueObject = new ValueObject_UsuariosNuevosBusqueda(queryType, value);
-			// ---------------------------------------------------------
-			System.out.println("MainWindow_Controller.btnBuscarFunction: Se creo un objectValue");
-			// ---------------------------------------------------------
+	 public DefaultTableModel btnBuscarFunction(int typeOfSearch, int queryType, String value) {
+			// queryType es un int que se utiliza para identificar
+			// si se utilizo la cedula venezolana o otro criterio
+			//columnNames guarda los nombres de las columnas a mostrar en la tabla
+			String[] columnNames = { "Cedula venezolana", "Primer nombre", "Apellido", "Motivo de Contacto" };
+			//Creo una tabla default a la cual no le agrego filas pero si columnas, las cuales esatn en ColumnNames
+			DefaultTableModel model = new TableModel(null, columnNames);
+			//Creo un array que guardara temporalmente los datos que van a ser agregados a el modelo
+			String[] rows = new String[4];
+			//Si los datos son del tipo correcto se ejecuta el bloque if
+			if (verifyInteger(value, queryType)) {
+				// este objeto guarda los datos y se pasan a facade
+				
+				ValueObject_UsuariosBusqueda valueObject = new ValueObject_UsuariosBusqueda(typeOfSearch, queryType, value);
+				
+				// ---------------------------------------------------------
+				System.out.println("MainWindow_Controller.btnBuscarFunction: Se creo un objectValue");
+				// ---------------------------------------------------------
 
-			// ---------------------------------------------------------
-			System.out.println("MainWindow_Controller.btnBuscarFunction: Se crea una nueva instancia de Facade y se intenta obtener configuracion");
-			// ---------------------------------------------------------
-			Facade facade = new Facade();
-			ResultSet recivedData;//objeto que guardara la informacion obtenida de la base de datos
-			// ---------------------------------------------------------
-			System.out.println("MainWindow_Controller.btnBuscarFunction: Se llamo a facade.UsuariosNuevosBusqueda");
-			// ---------------------------------------------------------
-			recivedData = facade.UsuariosNuevosBusqueda_Function(valueObject);
-			// ---------------------------------------------------------
-			System.out.println("MainWindow_Controller.btnBuscarFunction: Datos recibidos correctamente desde Facade.UsuariosNuevosBusqueda_Function");
-			// ---------------------------------------------------------
-			try {
-				while (recivedData.next()) {
-					//Inicio creacion de fila con datos de usuario
-					rows[0] = recivedData.getString("ci_venezolana");
-					rows[1] = recivedData.getString("primer_nombre");
-					rows[2] = recivedData.getString("primer_apellido");
-					rows[3] = recivedData.getString("motivo_contacto");
-					//termino creacion de fila con datos de usuario
-					model.addRow(rows);//Agrego la fila al modelo
-					
+				// ---------------------------------------------------------
+				System.out.println("MainWindow_Controller.btnBuscarFunction: Se crea una nueva instancia de Facade y se intenta obtener configuracion");
+				// ---------------------------------------------------------
+				Facade facade = new Facade();
+				ResultSet recivedData;//objeto que guardara la informacion obtenida de la base de datos
+				// ---------------------------------------------------------
+				System.out.println("MainWindow_Controller.btnBuscarFunction: Se llamo a facade.UsuariosNuevosBusqueda");
+				// ---------------------------------------------------------
+				recivedData = facade.UsuariosNuevosBusqueda_Function(valueObject);
+				// ---------------------------------------------------------
+				System.out.println("MainWindow_Controller.btnBuscarFunction: Datos recibidos correctamente desde Facade.UsuariosNuevosBusqueda_Function");
+				// ---------------------------------------------------------
+				try {
+					while (recivedData.next()) {
+						//Inicio creacion de fila con datos de usuario
+						rows[0] = recivedData.getString("ci_venezolana");
+						rows[1] = recivedData.getString("primer_nombre");
+						rows[2] = recivedData.getString("primer_apellido");
+						rows[3] = recivedData.getString("motivo_contacto");
+						//termino creacion de fila con datos de usuario
+						model.addRow(rows);//Agrego la fila al modelo
+						
+					}
+					return model;//Retorno modelo de tabla
+				} catch (SQLException e) {
+					e.printStackTrace();
+					return new DefaultTableModel();
 				}
-				return model;//Retorno modelo de tabla
-			} catch (SQLException e) {
-				e.printStackTrace();
+			}else{
 				return new DefaultTableModel();
 			}
-		}else{
-			return new DefaultTableModel();
 		}
-	}
 	public boolean verifyInteger(String value, int queryType) {
 		boolean partialResult = false;
 		try {
@@ -101,4 +104,21 @@ public class MainWindow_Controller {
 		formRegistro.setLocationRelativeTo(null);
 		formRegistro.setVisible(true);
 	}
+	public void MostrarInformacionDeUsuario(int ciVenezolana, String primerNombre, String primerApellido, String motivoContacto){
+		VerInformacion ventanaInformacion = new VerInformacion(ciVenezolana,primerNombre,primerApellido,motivoContacto);
+		ventanaInformacion.setLocationRelativeTo(null);
+		ventanaInformacion.setVisible(true);
+		
+	}
+	public void eliminarUsuario(int cedulaVenezolana, String primerNombre, String primerApellido){
+		int question = JOptionPane.showOptionDialog(null, "Realmente desea eliminar de los registros a: "+primerNombre+" "+primerApellido+"?", "Pregunta", 0,JOptionPane.YES_NO_OPTION, null, null, null);
+		System.out.println(question);
+		
+		
+		
+		
+		
+		
+	}
+	
 }
