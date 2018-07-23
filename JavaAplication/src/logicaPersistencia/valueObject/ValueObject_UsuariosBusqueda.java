96719aa768142a400a1778a04c8485a9d5185e81
Diego del Valle <diegodelvalle333@gmail.com>
2018-07-22 21:20:32 -0300

    Cambio de nombre

--- JavaAplication/src/logicaPersistencia/valueObject/ValueObject_UsuariosBusqueda.java
+++ JavaAplication/src/logicaPersistencia/valueObject/ValueObject_UsuariosBusqueda.java
@@ -1,19 +1,24 @@
 package logicaPersistencia.valueObject;
 
-public class ValueObject_UsuariosNuevosBusqueda {
+public class ValueObject_UsuariosBusqueda {
 	//ValueObject que contiene los datos y el tipo de la busqueda
 	//Este ValueObject Sera usado para realizar la busqueda en la bd
 	private int type; //tipo de busqueda
 	private String value; //Criterio de busqueda
-	public ValueObject_UsuariosNuevosBusqueda(int type, String value){
+	private int typeOfSearch;
+	public ValueObject_UsuariosBusqueda(int typeOfSearch, int type, String value){
 		this.type = type;
 		this.value = value;
+		this.typeOfSearch = typeOfSearch;
 	}	
 	public int getType() {
 		return type;
 	}
 	public String getValue() {
 		return value;
+	}
+	public int getTypeOfSearch(){
+		return typeOfSearch;		
 	}
 	
 }
