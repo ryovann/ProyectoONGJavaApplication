7bd85fd5f578ccf2c09e7ca9b02e948cecb6a501
Diego del Valle <diegodelvalle333@gmail.com>
2018-07-25 22:50:40 -0300

    Se agrego id persona y constructor para insertar documentos nuevos

--- JavaAplication/src/logicaPersistencia/valueObject/VO_Documentos.java
+++ JavaAplication/src/logicaPersistencia/valueObject/VO_Documentos.java
@@ -5,6 +5,7 @@ public class VO_Documentos {
 	private String ci_uruguaya;
 	private String pasaporte;
 	private String fecha_carnet_salud;
+	private int id_persona;
 	
 	public VO_Documentos(String ci_uruguaya, String pasaporte, String fecha_carnet_salud, String ci_venezolana) {
 		super();
@@ -20,6 +21,25 @@ public class VO_Documentos {
 		this.pasaporte = null;
 		this.fecha_carnet_salud = null;
 	}
+	public VO_Documentos(int id_persona,String ci_uruguaya, String pasaporte, String fecha_carnet_salud, String ci_venezolana) {
+		super();
+		//cambios aqui
+		this.ci_uruguaya = ci_uruguaya;
+		this.pasaporte = pasaporte;
+		this.fecha_carnet_salud = fecha_carnet_salud;
+		this.ci_venezolana=ci_venezolana;
+		this.id_persona = id_persona;
+	}
+	
+	public VO_Documentos (int id_persona,String ci_venezolana){
+		this.ci_venezolana=ci_venezolana;
+		//cambios aqui
+		this.ci_uruguaya = null;
+		this.pasaporte = null;
+		this.fecha_carnet_salud = null;
+		this.id_persona = id_persona;
+	}
+	
 	public String getCi_uruguaya() {
 		return ci_uruguaya;
 	}
@@ -34,5 +54,8 @@ public class VO_Documentos {
 	
 	public String getCi_venezolana(){
 		return ci_venezolana;
+	}
+	public int get_Id_persona(){
+		return id_persona;
 	}
 }
